815e7d00ce333b90b93464f644d29a75c6df4a19
advik77177-coder <advik77177@gmail.com>
2026-04-07 02:23:12 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ export async function search(query) {
   return [
     {
       title: query,
+      id: "1",
       magnet: "magnet:?xt=urn:btih:EXAMPLE"
     }
   ];
 }
 
 export async function play(item) {
-  return {
-    url: item.magnet
-  };
+  return [
+    {
+      url: item.magnet,
+      type: "torrent"
+    }
+  ];
 }
